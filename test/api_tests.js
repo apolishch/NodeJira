166b805a3b1bf2ea01ef352b86d5104925e1d912
Seth Carney <seth.carney@eloqua.com>
2012-08-14 13:59:31 -0400

    remove helpers and move to index. set up proper package structure. fix login and general processing. support multiple headers. etc

--- test/api_tests.js
+++ test/api_tests.js
@@ -1,9 +1,9 @@
-var should      = require('should'),
-    helper      = require("../jira_helpers")(process.env.JIRA_HOSTNAME, process.env.JIRA_USERNAME, process.env.JIRA_PASSWORD);
+var should = require('should'),
+    jira   = require('../lib/index')(process.env.JIRA_HOSTNAME, process.env.JIRA_USERNAME, process.env.JIRA_PASSWORD);
 
 describe('Api test', function() {
   it('should get record', function(done){
-    helper.get_issue('OEP-1234', function(err, response) {
+    jira.get_issue('OEP-1234', function(err, response) {
       should.not.exist(err);
       should.exist(response);
       done();
@@ -11,9 +11,11 @@ describe('Api test', function() {
   });
 
   it("should get back a list of issues", function(done){
-    helper.get_issues("project='test'", function(err, response) {
+    jira.get_issues("project='test'", function(err, response) {
+      if (err) return done(err);
       should.not.exist(err);
       should.exist(response);
+      response.total.should.not.equal(0);
       done();
     });
   });
